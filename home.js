7574a5f4fc6e07742efe990e78fb39a22b4ab80b
Tayna Alves <taynaalro@gmail.com>
2018-01-19 05:15:10 -0200

    homequase-ok

---
+++ home.js
@@ -0,0 +1,13 @@
+let frase = 'AGENDA CULTURAL';
+      let h1 = document.querySelector('h1');
+      let contador = 0;
+      function exibirLetra(){
+        let letra = frase[contador];
+        h1.innerHTML += letra;
+        contador++;
+        if(contador >= frase.length){
+          clearInterval(intervalo);
+
+        }
+      }
+      let intervalo = setInterval(exibirLetra, 230);
